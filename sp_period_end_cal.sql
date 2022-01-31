6fd2ffcb9576db465bece8e58f04d52ff9de73b3
Parthiv T <93038005+saitamatp@users.noreply.github.com>
2022-01-31 22:47:41 +0530

    Updated in case of no payment_id in misc_credit
    Added code to solve the issue when there is no payment in a particular payment_id.
    Ex -I recieved payment from x in form of cash, but I did not pay anything in form of cash. So in this case it wont be joined by previous SP code. Now this issue will be resolved.

--- sp_period_end_cal.sql
+++ sp_period_end_cal.sql
@@ -6,23 +6,28 @@ USE `ledger`$$
 PROCEDURE `sp_period_end_cal`()
 BEGIN
 create temporary table total as 
-select sum(amount_paid) as total ,period_id,paid_id from ledger.expense_detail
-group by period_id,paid_id;
+select sum(amount_paid) as total ,period_id,paid_id from ledger.expense_detail exp
+where exp.period_id in (select Period_id from ledger.period_detail c where curdate() between c.Starting_from and c.Ending_on)
+group by exp.period_id,exp.paid_id;
 
 create temporary table credit as 
-select sum(amount_paid) as total ,period_id,paid_id from ledger.misc_credit
-group by period_id,paid_id;
+select sum(amount_paid) as total ,period_id,paid_id from ledger.misc_credit cred
+where cred.period_id in (select Period_id from ledger.period_detail c where curdate() between c.Starting_from and c.Ending_on)
+group by cred.period_id,cred.paid_id;
 
 insert into expense_summary(amount,period_id,paid_id)
 select  
 ifnull((a.total-b.total),a.total) as total,a.period_id,a.paid_id
 from total a left join credit b
 on  a.period_id=b.period_id
- and a.paid_id=b.paid_id
-where a.period_id in (select Period_id from ledger.period_detail c where curdate() between c.Starting_from and c.Ending_on)
-group by a.period_id,a.paid_id;
+and a.paid_id=b.paid_id;
 
-
+/*Code added here*/
+insert into expense_summary(amount,period_id,paid_id)
+select sum(pp.total) as total ,pp.period_id,pp.paid_id from credit pp
+where not exists (select * from total cc where cc.paid_id =pp.paid_id)
+group by pp.paid_id,pp.period_id;
+/*Ends here*/
 
 drop temporary table total;
 drop temporary table credit;
