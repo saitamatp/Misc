a0a49be88965b0807a82e33066bc8a014a3e6509
Parthiv T <parthiv0306@gmail.com>
2022-04-11 19:48:54 +0530

    Updated the SP
    SP should run on the next month to process previous month data.

--- sp_period_end_cal.sql
+++ sp_period_end_cal.sql
@@ -1,18 +1,16 @@
-USE `ledger`;
-DROP procedure IF EXISTS `sp_period_end_cal`;
-
 DELIMITER $$
-USE `ledger`$$
-PROCEDURE `sp_period_end_cal`()
+CREATE DEFINER=`UserName`@`%` PROCEDURE `sp_period_end_cal`()
 BEGIN
+SET @period_id  = ((select Period_id from ledger.period_detail c where curdate() between c.Starting_from and c.Ending_on)-1);
+
 create temporary table total as 
 select sum(amount_paid) as total ,period_id,paid_id from ledger.expense_detail exp
-where exp.period_id in (select Period_id from ledger.period_detail c where curdate() between c.Starting_from and c.Ending_on)
+where exp.period_id = @period_id
 group by exp.period_id,exp.paid_id;
 
 create temporary table credit as 
 select sum(amount_paid) as total ,period_id,paid_id from ledger.misc_credit cred
-where cred.period_id in (select Period_id from ledger.period_detail c where curdate() between c.Starting_from and c.Ending_on)
+where cred.period_id = @period_id
 group by cred.period_id,cred.paid_id;
 
 insert into expense_summary(amount,period_id,paid_id)
@@ -22,17 +20,12 @@ from total a left join credit b
 on  a.period_id=b.period_id
 and a.paid_id=b.paid_id;
 
-/*Code added here*/
 insert into expense_summary(amount,period_id,paid_id)
 select sum(-pp.total) as total ,pp.period_id,pp.paid_id from credit pp
 where not exists (select * from total cc where cc.paid_id =pp.paid_id)
 group by pp.paid_id,pp.period_id;
-/*Ends here*/
 
 drop temporary table total;
 drop temporary table credit;
-
 END$$
-
 DELIMITER ;
-
