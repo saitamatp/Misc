b09d5da5e5351ba0bf1a8eb48660c063286d8a53
Parthiv T <Parthiv>
2022-04-26 15:59:14 +0530

    Updated to code to run more than once for the same period

--- sp_period_end_cal.sql
+++ sp_period_end_cal.sql
@@ -1,31 +1,60 @@
-DELIMITER $$
-CREATE DEFINER=`UserName`@`%` PROCEDURE `sp_period_end_cal`()
+CREATE DEFINER
+=`Kat`@`%` PROCEDURE `sp_period_end_cal`
+()
+sp:
 BEGIN
-SET @period_id  = ((select Period_id from ledger.period_detail c where curdate() between c.Starting_from and c.Ending_on)-1);
+    SET @period_id  = ((select Period_id
+    from ledger.period_detail c
+    where curdate() between c.Starting_from and c.Ending_on)-1);
 
-create temporary table total as 
-select sum(amount_paid) as total ,period_id,paid_id from ledger.expense_detail exp
+    set @count_record=(select period_id
+    from ledger.expense_summary
+    where period_id=@period_id
+    group by period_id);
+
+    if @count_record is not null then
+
+    set @id=(select max(id)
+    from error_message.sp_period_end_cal);
+
+    insert into error_message.sp_period_end_cal
+        (id,error_date,message)
+    values(@id+1, now(), 'Running more than once for the same period');
+
+    leave sp;
+end
+if;
+
+create temporary table total as
+select sum(amount_paid) as total , period_id, paid_id
+from ledger.expense_detail exp
 where exp.period_id = @period_id
 group by exp.period_id,exp.paid_id;
 
-create temporary table credit as 
-select sum(amount_paid) as total ,period_id,paid_id from ledger.misc_credit cred
+create temporary table credit as
+select sum(amount_paid) as total , period_id, paid_id
+from ledger.misc_credit cred
 where cred.period_id = @period_id
 group by cred.period_id,cred.paid_id;
 
-insert into expense_summary(amount,period_id,paid_id)
-select  
-ifnull((a.total-b.total),a.total) as total,a.period_id,a.paid_id
+insert into expense_summary
+    (amount,period_id,paid_id)
+select
+    ifnull((a.total-b.total),a.total) as total, a.period_id, a.paid_id
 from total a left join credit b
-on  a.period_id=b.period_id
-and a.paid_id=b.paid_id;
+    on  a.period_id=b.period_id
+        and a.paid_id=b.paid_id;
 
-insert into expense_summary(amount,period_id,paid_id)
-select sum(-pp.total) as total ,pp.period_id,pp.paid_id from credit pp
-where not exists (select * from total cc where cc.paid_id =pp.paid_id)
+insert into expense_summary
+    (amount,period_id,paid_id)
+select sum(-pp.total) as total , pp.period_id, pp.paid_id
+from credit pp
+where not exists (select *
+from total cc
+where cc.paid_id =pp.paid_id)
 group by pp.paid_id,pp.period_id;
 
 drop temporary table total;
 drop temporary table credit;
-END$$
-DELIMITER ;
+
+END
